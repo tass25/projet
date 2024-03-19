3877fe72215e5e859b6df2f71e7a5f7112f02764
tass25 <101891297+tass25@users.noreply.github.com>
2024-03-19 12:32:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -15,55 +15,7 @@ document.querySelectorAll('.about .video-container .controls .control-btn').forE
     };
 });
 
-// ***************LOGIN & REGISTER***************
-
-// Sélection et configuration du popup de connexion
-let loginPopup = document.getElementById('login-popup');
-let loginBtn = document.getElementById('login-btn1');
-let closePopupBtn = document.getElementById('close-popup');
-
-// Ouvre le popup de connexion lorsque le bouton de connexion est cliqué
-loginBtn.onclick = () => {
-    loginPopup.style.display = 'flex';
-};
-
-// Ferme le popup de connexion lorsque le bouton de fermeture est cliqué
-closePopupBtn.onclick = () => {
-    loginPopup.style.display = 'none';
-};
-
-// Ferme le popup de connexion en cliquant en dehors du popup
-window.onclick = (event) => {
-    if (event.target === loginPopup) {
-        loginPopup.style.display = 'none';
-    }
-};
-
-// Gestion de la soumission du formulaire de connexion
-let loginFormPopup = document.getElementById('login-form-popup');
-loginFormPopup.addEventListener('submit', (event) => {
-    event.preventDefault();
-    alert('Connexion réussie !');
-    loginPopup.style.display = 'none';
-});
-
-// Gestion de la soumission du formulaire d'inscription
-document.getElementById('registerForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Empêche la soumission automatique du formulaire
-    // Récupération et validation des champs du formulaire
-    var fn = document.forms["registerForm"]["firstName"].value;
-    var ln = document.forms["registerForm"]["lastName"].value;
-    var bd = document.forms["registerForm"]["birthDate"].value;
-    var ph = document.forms["registerForm"]["phone"].value;
-    var em = document.forms["registerForm"]["email"].value;
-    var pwd = document.forms["registerForm"]["password"].value;
-    var cpwd = document.forms["registerForm"]["confirmPassword"].value;
-    // Validation des champs
-    if (fn === "" || ln === "" || bd === "" || ph === "" || em === "" || pwd === "" || cpwd === "") {
-        alert("Tous les champs doivent être remplis");
-        return false;
-    }
-});
+// ****************LOGIN & REGISTER****************
 
 // Redirection après soumission du formulaire d'inscription
 document.addEventListener('DOMContentLoaded', (event) => {
