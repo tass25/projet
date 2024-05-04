a62064196a465c4e8acfa17e0d41bf89b26bf758
tass <laboutesse@gmail.com>
2024-05-04 13:00:45 +0200

    logout

---
+++ deconnexion.php
@@ -0,0 +1,7 @@
+<?php
+session_start();
+session_unset();
+session_destroy();
+header('location:register.php');
+//ki ta3ml bochra el avatar ki nenzl alih nalkaw my settings , my profile w logout , ken e logout tekhdm , ki tnzl aleha taml e traitement hedha 
+?>
